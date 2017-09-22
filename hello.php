4c1e11443450f568ae59dd0188eb928e96ae0c08
lpljn <399129811@qq.com>
2017-09-22 09:22:34 +0800

    five hundred miles

--- hello.php
+++ hello.php
@@ -1,8 +1,5 @@
 hello world!
-<<<<<<< HEAD
+
 my boss is stupid!`
 Creating a branch is quick and simple!
-=======
-my boss is stupid!
-Creating a branch is quick and simple!
->>>>>>> feature
+five hundred miles!
