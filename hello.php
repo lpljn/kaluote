b9ff0e237d7364095e61503f850224007828827c
lpljn <399129811@qq.com>
2017-09-22 10:06:13 +0800

    主干合分支

--- hello.php
+++ hello.php
@@ -1,11 +1,6 @@
 hello world!
 
-<<<<<<< HEAD
 my boss is stupid!
 Creating a branch is quick and simple!
 five hundred miles!
-=======
-my boss is stupid!`
-Creating a branch is quick and simple!
-five hundred miles!
->>>>>>> dev
+
